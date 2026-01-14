e66142715bbc28b0194fdab95de16f567f4d14b4
JoelAlgera <Joel.Algera@student.ufv.ca>
2026-01-14 10:30:32 -0800

    Mini commit
    Started working on collecting all the data and making the function to rank the universities. I got 3 done and made a rough function which can easily be tweaked. Just have to go to class now, uploading to github so i can work on it from my laptop.

--- ranking.java
+++ ranking.java
@@ -3,26 +3,25 @@ import java.util.ArrayList;
 public class ranking {
 public static void main(String[] args) {
     ArrayList<entry> entries = new ArrayList<>();
-    entries.add(new entry(150, 2, "bob", "Dr. Smith", "Algorithms 101", "Tech University", 5));
-    entries.add(new entry(150, 2, "bob", "Dr. Smith", "Algorithms 101", "Tech University", 2));
-    entries.add(new entry(150, 2, "bob", "Dr. Smith", "Algorithms 101", "Tech University", 50));
-    entries.add(new entry(150, 2, "bob", "Dr. Smith", "Algorithms 101", "Tech University", 10));
-    entries.add(new entry(150, 2, "bob", "Dr. Smith", "Algorithms 101", "Tech University", 15));
-
+    entries.add(new entry(1030, 1, "Algorithms, 4th Edition by Robert Sedgewick and Kevin Wayne", "Marcel dall'Agnol, Maryam Hedayati, Kevin Wayne", "COS 226", "Princeton University", 24));
+    entries.add(new entry(1505, 3, "Tim Roughgarden, Algorithms Illuminated, Volumes I, II, and III", "Mary Wootters", "CS161", "Stanford University", 17));
+    entries.add(new entry(439, 1, "Introduction to Algorithms by Cormen, Leiserson, Rivest, and Stein", "Erik Demaine, Srini Devadas, Nancy Lynch", "Design and Analysis of Algorithms", "MIT", 24));
     sort(entries);
-    for (entry e : entries) {
-        System.out.println(e.toString());
+
+
+    for (int i = 0; i < entries.size(); i++) {
+        System.out.println(entries.get(entries.size()-i-1).toString());
     }
 
 }
 
     static void sort(ArrayList<entry> list){
         int n = list.size();
-        for (int i = 1; i < n - 1; i++) {
+        for (int i = 1; i < n; i++) {
             entry key = list.get(i);
             int j = i - 1;
 
-            while (j >= 0 && list.get(j).universityRank > key.universityRank) {
+            while (j >= 0 && list.get(j).courseRank > key.courseRank) {
                 list.set(j+1, list.get(j));
                 j--;
             }
@@ -39,19 +38,22 @@ class entry {
     String professorName;
     String courseName;
     String universityName;
-    int universityRank;
+    int lectureCount;
+    int courseRank;
 
-    public entry(int slides, int textbookCount, String textbooks, String professorName, String courseName, String universityName, int universityRank) {
+    public entry(int slides, int textbookCount, String textbooks, String professorName, String courseName, String universityName, int lectureCount) {
         this.slides = slides;
         this.textbookCount = textbookCount;
         this.textbooks = textbooks;
         this.professorName = professorName;
         this.courseName = courseName;
         this.universityName = universityName;
-        this.universityRank = universityRank;
+        this.lectureCount = lectureCount;
+        this.courseRank = (int)((slides/lectureCount) * textbookCount);
     }
 
     public String toString() {
-        return "Course: " + courseName + ", Professor: " + professorName + ", University: " + universityName + " (Rank: " + universityRank + "), Slides: " + slides + ", Textbooks: " + textbookCount;
+        return("courseRank: " + courseRank);
+        //return " (Rank: " + courseRank + "), Course: " + courseName + ", Professor: " + professorName + ", University: " + universityName +  ", Slides: " + slides + ", Textbooks: " + textbookCount;
     } 
 }
